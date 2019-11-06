b9a7bd25e9903737421b21712500b93e65515987
ir4klis <ir4klis@gmail.com>
2019-11-07 00:13:33 +0200

    add local js/css folders add i18n strings to main function

--- cookie.php
+++ cookie.php
@@ -1,12 +1,12 @@
 <?php
 /*
 *
-*Plugin Name: Cookie
+*Plugin Name: Simple Cookie
 *Description: Add Cookie consent popup to my thesis' site
 *Version: 0.1
 *Author: Heracles Michailidis
 *Author URI: ir4klis.github.io
-*Text Domain: thesis-cookie
+*Text Domain: simplecookie
 *Domain Path: /languages
 *
 */
@@ -15,7 +15,9 @@ function hook_cookie_popup() {
     ?>
         <link rel="stylesheet" type="text/css" href="//wpcc.io/lib/1.0.2/cookieconsent.min.css"/>
         <script src="//wpcc.io/lib/1.0.2/cookieconsent.min.js"></script>
-        <script>window.addEventListener("load", function(){window.wpcc.init({"colors":{"popup":{"background":"#222222","text":"#ffffff","border":"#b3d0e4"},"button":{"background":"#7b417a","text":"#ffffff"}},"position":"bottom","content":{"href":" http://exam.devguides.net/privacy-policy/","message":"Αυτός ο ιστότοπος χρησιμοποιεί cookies για να παρέχει τις υπηρεσίες του.","link":"Περισσότερα","button":"Εντάξει"}})});</script>
+        <script>
+        window.addEventListener("load", function(){window.wpcc.init({"colors":{"popup":{"background":"#222222","text":"#ffffff","border":"#b3d0e4"},"button":{"background":"#7b417a","text":"#ffffff"}},"position":"bottom","content":{"href":" http://exam.devguides.net/privacy-policy/","message":"<?php _e('This site uses cookies to provide its services.','simplecookie') ?>","link":"<?php _e('More','simplecookie') ?>","button":"<?php _e('OK','simplecookie') ?>"}})});
+        </script>
     <?php
 }
 add_action('wp_head', 'hook_cookie_popup');
